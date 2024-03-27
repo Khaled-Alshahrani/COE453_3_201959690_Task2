c6e7407e2cea1d1197f641ed1cbd72bea432b56d
khaled-alshahrani <ksh-0-@hotmail.com>
2024-03-27 18:52:06 +0300

    final

---
+++ add/add.js
@@ -0,0 +1,16 @@
+const functions = require("@google-cloud/functions-framework");
+
+functions.http("add", (req, res) => {
+  const num1 = parseInt(req.query.X);
+  const num2 = parseInt(req.query.Y);
+
+  const result = num1 + num2;
+
+  const response = {
+    X: num1,
+    Y: num2,
+    result: result,
+  };
+
+  res.json(response);
+});
